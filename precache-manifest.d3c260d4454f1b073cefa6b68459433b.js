37dacc8689fdab547cccb13e9f11c3bff3a64fa2
Shruti <shrutishastri49@gmail.com>
2019-08-25 22:32:53 +0530

    Updates

---
+++ precache-manifest.d3c260d4454f1b073cefa6b68459433b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "26cacf8ef6691c909269",
+    "url": "/BOOK-SHELF-APP/static/css/main.c54ec075.chunk.css"
+  },
+  {
+    "revision": "26cacf8ef6691c909269",
+    "url": "/BOOK-SHELF-APP/static/js/main.26cacf8e.chunk.js"
+  },
+  {
+    "revision": "b1fd4a6d297c5f3fd8f7",
+    "url": "/BOOK-SHELF-APP/static/js/1.b1fd4a6d.chunk.js"
+  },
+  {
+    "revision": "dc1a17b4a19317bbc403",
+    "url": "/BOOK-SHELF-APP/static/js/runtime~main.dc1a17b4.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/BOOK-SHELF-APP/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/BOOK-SHELF-APP/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/BOOK-SHELF-APP/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "0767393d138687e069d94f15548500be",
+    "url": "/BOOK-SHELF-APP/index.html"
+  }
+];
